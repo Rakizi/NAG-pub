1bdb3b4a0f771deeefb0e34a77eb848476fbcdf5
Rakizi <>
2025-07-03 12:51:43 +0000

    Update from https://github.com/Rakizi/NextActionGuide/commit/c00992f7b5248215026abe16660721877115a0c7

--- handlers/APL_Core.lua
+++ handlers/APL_Core.lua
@@ -97,7 +97,7 @@ local EXECUTE_SPELLS = {
     --[114866] = 35,  -- Soul Reaper (Death Knight)
 }
 
---- Checks if a spell can be executed on the current target, including special logic for Touch of Death.
+--- Checks if a spell can be executed on the current target, including special logic for Touch of Death/Hammer of Wrath
 --- @param spellId number The spell ID to check.
 --- @param unit string The unit to check (default: "target").
 --- @return boolean True if the spell can be executed, false otherwise.
